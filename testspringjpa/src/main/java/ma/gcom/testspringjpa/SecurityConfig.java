e3e78c79ec895b86cf98a428b5654d701c8495a7
azdad pavilion <azdadanass@gmail.com>
2020-08-24 14:15:49 +0100

    sample SecurityConfig.configure

--- testspringjpa/src/main/java/ma/gcom/testspringjpa/SecurityConfig.java
+++ testspringjpa/src/main/java/ma/gcom/testspringjpa/SecurityConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
@@ -14,6 +15,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	DataSource dataSource;
+
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/**").hasRole("USER").and().formLogin().loginProcessingUrl("/login").defaultSuccessUrl("/user", true);
+	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
